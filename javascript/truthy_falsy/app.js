c88682aaaacd91bf5d9a5db340fa9c387cc4d282
tuzyLevel <pge0823@gmail.com>
2022-07-16 15:14:55 +0900

    js, node-practice-07161514

--- javascript/truthy_falsy/app.js
+++ javascript/truthy_falsy/app.js
@@ -6,8 +6,17 @@
 //   console.log("falsy");
 // }
 
-if (null) {
-  console.log("t");
-} else {
-  console.log("f");
+// const password = prompt("Enter your password");
+
+// if (password.length >= 6 && !password.includes(" ")) {
+//   console.log("VALID PASSWORD");
+// } else {
+//   console.log("INVALID");
+// }
+
+const age = 90;
+
+if ((0 <= age && age < 5) || age >= 65) {
+  console.log("FREE");
+} else if (age < 10) {
 }
