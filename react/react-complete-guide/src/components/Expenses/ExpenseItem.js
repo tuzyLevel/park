7e5313dbf8c914f80de8202837f0a600a81f2a37
tuzyLevel <pge0823@gmail.com>
2022-07-24 12:09:59 +0900

    react-practice-07241209

---
+++ react/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+const ExpenseItem = ({ date, amount, title }) => {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
